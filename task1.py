e21cf568a07526f69fd55080431dc79b4537548b
Dmytro Andrushko <dmytro.andrushko@gmail.com>
2025-04-02 10:29:55 +0300

    PZ on 2 of April

--- task1.py
+++ task1.py
@@ -1,26 +1,31 @@
 # 1. Отримати курси евро за попередній тиждень, вивести на екран дату + курс
 # 2. З отриманого словника побудувати графк зміни курсу за тиждень
+# URL for request https://bank.gov.ua/NBU_Exchange/exchange_site?start=20250317&end=20250321&valcode=eur&json
 import json
 import requests
 
-# URL for request https://bank.gov.ua/NBU_Exchange/exchange_site?start=20250317&end=20250321&valcode=eur&json
-response_data = requests.get("https://bank.gov.ua/NBU_Exchange/exchange_site?start=20250317&end=20250321&valcode=eur&json")
+nbu_response = requests.get("https://bank.gov.ua/NBU_Exchange/exchange_site?start=20250301&end=20250328&valcode=eur&json")
 
-response_list = json.loads(response_data.content)
+# print(nbu_response)
+# print(nbu_response.content)
 
-exchange_date = []
-exchange_rate = []
-# Logic to extract data from NBU service response
+converted_response = json.loads(nbu_response.content)
+# print(converted_response)
 
+exchange_dates = []
+exchange_rates = []
 
-### Part 2
+for item in converted_response:
+    exchange_dates.append(item['exchangedate'])
+    exchange_rates.append(item['rate'])
+
+print(exchange_rates)
+print(exchange_dates)
 # Matplotlib
-# import matplotlib.pyplot as plt
-#
-# plt.plot(exchange_date, exchange_rate)
-# plt.show()
 
+import matplotlib.pyplot as plt
 
-
+plt.plot(exchange_dates, exchange_rates)
+plt.show()
 
 
