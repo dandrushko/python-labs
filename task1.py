841df8d3f291321b131f673e0d5d20195523504a
Dmytro Andrushko <dmytro.andrushko@gmail.com>
2025-04-02 10:31:41 +0300

    PZ on 2 of April

--- task1.py
+++ task1.py
@@ -6,26 +6,14 @@ import requests
 
 nbu_response = requests.get("https://bank.gov.ua/NBU_Exchange/exchange_site?start=20250301&end=20250328&valcode=eur&json")
 
-# print(nbu_response)
-# print(nbu_response.content)
 
 converted_response = json.loads(nbu_response.content)
-# print(converted_response)
-
-exchange_dates = []
-exchange_rates = []
 
 for item in converted_response:
-    exchange_dates.append(item['exchangedate'])
-    exchange_rates.append(item['rate'])
+    #Go through a loop here
 
-print(exchange_rates)
-print(exchange_dates)
 # Matplotlib
 
 import matplotlib.pyplot as plt
-
-plt.plot(exchange_dates, exchange_rates)
-plt.show()
 
 
